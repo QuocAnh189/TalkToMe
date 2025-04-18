139afeacf015e339d76f939ef705941409f14cd3
QuocAnh189 <anhquoc18092003@gmail.com>
2025-04-18 22:50:48 +0700

    update database

--- internal/infrashstructrure/persistence/db/db.go
+++ internal/infrashstructrure/persistence/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -13,7 +14,30 @@ const (
 )
 
 type IDatabase interface {
+	GetDB() *gorm.DB
 	AutoMigrate(models ...any) error
+	WithTransaction(function func() error) error
+	Create(ctx context.Context, doc any) error
+	CreateInBatches(ctx context.Context, docs any, batchSize int) error
+	Update(ctx context.Context, doc any) error
+	Delete(ctx context.Context, value any, opts ...FindOption) error
+	DeleteById(ctx context.Context, model any, id string) error
+	FindById(ctx context.Context, id string, result any) error
+	FindOne(ctx context.Context, result any, opts ...FindOption) error
+	Find(ctx context.Context, result any, opts ...FindOption) error
+	Count(ctx context.Context, model any, total *int64, opts ...FindOption) error
+}
+
+type Query struct {
+	Query string
+	Args  []any
+}
+
+func NewQuery(query string, args ...any) Query {
+	return Query{
+		Query: query,
+		Args:  args,
+	}
 }
 
 type Database struct {
@@ -43,4 +67,150 @@ func NewDatabase(uri string) (*Database, error) {
 
 func (d *Database) AutoMigrate(models ...any) error {
 	return d.db.AutoMigrate(models...)
+}
+
+func (d *Database) WithTransaction(function func() error) error {
+	callback := func(db *gorm.DB) error {
+		return function()
+	}
+
+	tx := d.db.Begin()
+	if err := callback(tx); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) Preload(query string, args ...interface{}) IDatabase {
+	d.db.Preload(query, args...)
+	return d
+}
+
+func (d *Database) Create(ctx context.Context, doc any) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	return d.db.Create(doc).Error
+}
+
+func (d *Database) CreateInBatches(ctx context.Context, docs any, batchSize int) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	return d.db.CreateInBatches(docs, batchSize).Error
+}
+
+func (d *Database) Update(ctx context.Context, doc any) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	return d.db.Save(doc).Error
+}
+
+func (d *Database) Delete(ctx context.Context, value any, opts ...FindOption) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	query := d.applyOptions(opts...)
+	return query.Delete(value).Error
+}
+
+func (d *Database) DeleteById(ctx context.Context, model any, id string) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	if err := d.db.Delete(model, id).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) FindById(ctx context.Context, id string, result any) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	if err := d.db.Where("id = ? ", id).First(result).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) FindOne(ctx context.Context, result any, opts ...FindOption) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	query := d.applyOptions(opts...)
+	if err := query.First(result).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) Find(ctx context.Context, result any, opts ...FindOption) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	query := d.applyOptions(opts...)
+	if err := query.Find(result).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) Count(ctx context.Context, model any, total *int64, opts ...FindOption) error {
+	ctx, cancel := context.WithTimeout(ctx, DatabaseTimeout)
+	defer cancel()
+
+	query := d.applyOptions(opts...)
+	if err := query.Model(model).Count(total).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Database) GetDB() *gorm.DB {
+	return d.db
+}
+
+func (d *Database) applyOptions(opts ...FindOption) *gorm.DB {
+	query := d.db
+
+	opt := getOption(opts...)
+
+	if len(opt.preloads) != 0 {
+		for _, preload := range opt.preloads {
+			query = query.Preload(preload)
+		}
+	}
+
+	if opt.query != nil {
+		for _, q := range opt.query {
+			query = query.Where(q.Query, q.Args)
+		}
+	}
+
+	if opt.order != "" {
+		query = query.Order(opt.order)
+	}
+
+	if opt.offset != 0 {
+		query = query.Offset(opt.offset)
+	}
+
+	if opt.limit != 0 {
+		query = query.Limit(opt.limit)
+	}
+
+	return query
 }
